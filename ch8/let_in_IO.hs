4491e28103e64b2d39f24e2ad36b1e0eb7487744
Yu Hoshino <yuhoshino@dev.local>
2015-09-09 00:53:37 +0900

    close #8

---
+++ ch8/let_in_IO.hs
@@ -0,0 +1,12 @@
+import Data.Char
+
+main = do
+  putStrLn "What's your first name?"
+  firstName <- getLine
+  putStrLn "What's your last name?"
+  lastName <- getLine
+  let bigFirstName = map toUpper firstName
+      bigLastName  = map toUpper lastName
+  putStrLn $ "Hey " ++ bigFirstName ++ " "
+                    ++ bigLastName
+                    ++ ", how is it going?"
---
+++ ch8/sequence.hs
@@ -0,0 +1,9 @@
+{- main = do -}
+  {- a <- getLine -}
+  {- b <- getLine -}
+  {- c <- getLine -}
+  {- print [a,b,c] -}
+
+main = do
+  rs <- sequence [getLine, getLine, getLine]
+  print rs
